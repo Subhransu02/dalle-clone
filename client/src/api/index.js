af70698aa5c7bc94758bcc3635f9fd26300260f1
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2024-02-17 19:07:36 +0530

    server api updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8080/api/" });
+const API = axios.create({ baseURL: "https://ai-image-generator-awhj.onrender.com/api/" });
 
 export const GetPosts = async() => await API.get("/post/");
 export const CreatePost = async(data) => await API.post("/post/", data);
