8f97c6600738373fd173e169bd481f9c0b9c8724
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2024-02-17 14:54:20 +0530

    add models and routes

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({ extended: true}));
+
+//error handler
+app.use((err, req, res, next)=>{
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong";
+    return res.status(status).json({
+        success:false,
+        status,
+        message,
+    });
+})
+
+//default get
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        message: "Hello",
+    });
+});
+
+const connectDB = async () => {
+    mongoose.set("strictQuery", true);
+    mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((e) => console.log(`Error connecting to MongoDB: ${e}`));
+}
+
+const startServer = async () => {
+    try{
+        connectDB()
+        app.listen(8080, () => console.log("Server is running on port 8080"));
+    }catch(error){
+        console.log(error);
+    }
+}
+
+startServer();
