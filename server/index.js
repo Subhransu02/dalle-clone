6a364534d55a12b9694f10490c111d05bab8cd89
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2024-02-17 15:48:39 +0530

    add generate ai image, open ai

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import PostRouter from "./routes/Posts.js";
+import  GenerateImageRouter  from "./routes/GenerateImage.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use("/api/post", PostRouter);
+app.use("/api/generateImage", GenerateImageRouter);
 
 //default get
 app.get("/", async (req, res) => {
